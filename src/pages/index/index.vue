1924e56f32032c509b478dad3f6be7eebf69014c
sun-xue-shi <2595531421@qq.com>
2023-11-13 15:08:42 +0800

    feat：轮播图组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import CustomNavbar from '@/components/CustomNavbar.vue'
+import ShopSwiper from '@/components/ShopSwiper.vue'
 </script>
 
 <template>
   <CustomNavbar />
+  <ShopSwiper />
   <view class="index">首页</view>
 </template>
 
